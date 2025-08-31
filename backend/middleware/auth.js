cdd02e7bd26204947095924698f3a0be2bd31349
Meenakshi-Dubbaka <meenakshi.dubbaka16@gmail.com>
2025-08-31 22:39:17 +0530

    fixed backend

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
     const token=req.cookies.token;
     if(!token) return res.status(401).json({message:"Not authenticated"});
     jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
-        if(err) return res.status(403).json({message:"Invalid tokrn"});
+        if(err) return res.status(403).json({message:"Invalid token"});
         req.user=decoded;
         next();
     })
